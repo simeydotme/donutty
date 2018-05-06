a69ee14c24508f5d4536aebdea5794c05f650e23
simeydotme <simey.me@gmail.com>
2018-05-06 18:15:15 +0800

    add minify versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 
 var gulp = require( "gulp" ),
-    concat = require( "gulp-concat" );
+    concat = require( "gulp-concat" ),
+    rename = require( "gulp-rename" ),
+    uglify = require( "gulp-uglify" );
 
 gulp.task( "vanilla", function() {
 
   gulp.src( ["./src/donutty.js", "./src/vanilla.js"] )
     .pipe( concat( "donutty.js" ) )
+    .pipe( gulp.dest( "./dist" ) )
+    .pipe( uglify() )
+    .pipe( rename( "donutty.min.js" ) )
     .pipe( gulp.dest( "./dist" ) );
 
 });
@@ -14,6 +19,9 @@ gulp.task( "jquery", function() {
 
   gulp.src( ["./src/donutty.js", "./src/jquery.js"] )
     .pipe( concat( "donutty-jquery.js" ) )
+    .pipe( gulp.dest( "./dist" ) )
+    .pipe( uglify() )
+    .pipe( rename( "donutty-jquery.min.js" ) )
     .pipe( gulp.dest( "./dist" ) );
 
 });
